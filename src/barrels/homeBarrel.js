cb1dfcc71d49f51044406502c4fe2246a29c164d
xero-lib <jonathankurtz@protonmail.com>
2020-11-28 04:57:21 -0600

    Implemented basic Rift UI (removed Home), modularized

---
+++ src/barrels/homeBarrel.js
@@ -0,0 +1,2 @@
+export {default as Sidebar} from '../components/Sidebar/Sidebar';
+export {default as RiftHome} from '../components/Home/RiftHome';
---
+++ src/components/Home/RiftHome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../css/index.css';
+
+export default class RiftHome extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>What is Rift?</h1>
+        <p>
+          Rift is a highly customizable, versatile, and open source communications and cooridnation platform for everyone, from the casual texter, to corporate level team management.
+        </p>
+        <p2>
+          The current developers are Jonathan Kurtz and Skyler Spaeth.
+        </p2>
+      </main>
+    );
+  }
+}
