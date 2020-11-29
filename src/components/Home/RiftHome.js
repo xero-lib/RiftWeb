14130ed9138c2d2069590990da89f092f722b018
xero-lib <jonathankurtz@protonmail.com>
2020-11-28 19:34:40 -0600

    Official checkpoint

--- src/components/Home/RiftHome.js
+++ src/components/Home/RiftHome.js
@@ -5,13 +5,13 @@ export default class RiftHome extends React.Component {
   render() {
     return (
       <main>
-        <h1>What is Rift?</h1>
-        <p>
+        <h1 class="title">What is Rift?</h1>
+        <h3 class="description">
           Rift is a highly customizable, versatile, and open source communications and cooridnation platform for everyone, from the casual texter, to corporate level team management.
-        </p>
-        <p2>
-          The current developers are Jonathan Kurtz and Skyler Spaeth.
-        </p2>
+        </h3>
+        <footer class="footer">
+          The current developers are <a href='https://github.com/xero-lib'>Jonathan Kurtz</a> and <a href='https://github.com/skylerspaeth'>Skyler Spaeth</a>.
+        </footer>
       </main>
     );
   }
